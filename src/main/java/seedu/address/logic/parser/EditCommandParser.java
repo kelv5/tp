68cd8102323a5ce8893e83e0dbcf121d5ce744c7
kelv5 <ngkelvinkahkit@gmail.com>
2026-03-30 23:46:10 +0800

    Refactor EditCommandParser for code duplication

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -13,6 +13,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Consumer;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.EditCommand;
@@ -26,18 +27,25 @@ import seedu.address.model.tag.Tag;
 public class EditCommandParser implements Parser<EditCommand> {
 
     /**
+     * Functional interface to represent a function that converts a string into object of type T.
+     */
+    @FunctionalInterface
+    private interface StringParser<T> {
+        // Solution below inspired by https://stackoverflow.com/a/73036423.
+        T parse(String input) throws ParseException;
+    }
+
+    /**
      * Parses the given {@code String} of arguments in the context of the EditCommand
      * and returns an EditCommand object for execution.
-     * @throws ParseException if the user input does not conform the expected format
+     * @throws ParseException if the user input does not conform the expected format.
      */
     public EditCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
-                        PREFIX_ADDRESS, PREFIX_TAG, PREFIX_TELEGRAM);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(
+                args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG, PREFIX_TELEGRAM);
 
         Index index;
-
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
@@ -49,28 +57,40 @@ public class EditCommandParser implements Parser<EditCommand> {
 
         EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
 
-        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
-            editPersonDescriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
-        }
-        if (argMultimap.getValue(PREFIX_PHONE).isPresent()) {
-            editPersonDescriptor.setPhone(ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get()));
-        }
-        if (argMultimap.getValue(PREFIX_EMAIL).isPresent()) {
-            editPersonDescriptor.setEmail(ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get()));
-        }
-        if (argMultimap.getValue(PREFIX_ADDRESS).isPresent()) {
-            editPersonDescriptor.setAddress(ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get()));
-        }
+        setFieldIfPresent(argMultimap.getValue(PREFIX_NAME),
+                editPersonDescriptor::setName, ParserUtil::parseName);
+
+        setFieldIfPresent(argMultimap.getValue(PREFIX_PHONE),
+                editPersonDescriptor::setPhone, ParserUtil::parsePhone);
+
+        setFieldIfPresent(argMultimap.getValue(PREFIX_EMAIL),
+                editPersonDescriptor::setEmail, ParserUtil::parseEmail);
+
+        setFieldIfPresent(argMultimap.getValue(PREFIX_ADDRESS),
+                editPersonDescriptor::setAddress, ParserUtil::parseAddress);
+
+        setFieldIfPresent(argMultimap.getValue(PREFIX_TELEGRAM),
+                editPersonDescriptor::setTelegram, ParserUtil::parseTelegram);
+
         parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(editPersonDescriptor::setTags);
-        if (argMultimap.getValue(PREFIX_TELEGRAM).isPresent()) {
-            editPersonDescriptor.setTelegram(ParserUtil.parseTelegram(argMultimap.getValue(PREFIX_TELEGRAM).get()));
-        }
 
         if (!editPersonDescriptor.isAnyFieldEdited()) {
             throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
         }
 
         return new EditCommand(index, editPersonDescriptor);
+    }
+
+    /**
+     * Sets a field in the {@code EditPersonDescriptor} if a value is present.
+     * If {@code Optional<String> value} is present, it will be parsed and passed into the updater.
+     */
+    private <T> void setFieldIfPresent(Optional<String> value, Consumer<T> fieldUpdater,
+            StringParser<T> parser) throws ParseException {
+
+        if (value.isPresent()) {
+            fieldUpdater.accept(parser.parse(value.get()));
+        }
     }
 
     /**
